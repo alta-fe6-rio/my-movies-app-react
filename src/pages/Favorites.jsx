94e8a2960060d17932ec454580386e381a90d559
Rio Dwi S <riodwisyahputra25@gmail.com>
2022-07-30 14:33:50 +0700

    add remove movies fav

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,23 +2,33 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import Swal from 'sweetalert2';
 
 import Layout from '../components/Layout';
 import { MovieCardFav } from '../components/MovieCard';
 import { withRouter } from '../utils/navigation';
-import Swal from 'sweetalert2';
+import { useDispatch } from 'react-redux';
+import { reduxAction } from '../utils/redux/action/action';
 
 import '../styles/App.css';
 
 const Homepage = (props) => {
+	const dispatch = useDispatch();
 	const favorites = useSelector((state) => state.favorites);
 	const navigate = useNavigate();
 
-	const RemoveFav = () => {
+	const RemoveFav = (index) => {
+		const tempLocal = localStorage.getItem('favMovie');
+		const temp = JSON.parse(tempLocal);
+		temp.splice(index, 1);
+		localStorage.setItem('favMovie', JSON.stringify(temp));
+		dispatch(reduxAction('REMOVE_FAVORITE', temp));
 		Swal.fire({
-			icon: 'info',
-			title: 'feature on development',
-			showCloseButton: true,
+			icon: 'success',
+			title: 'Success',
+			html: '<p>Movies has been removed</p>' + '<p>Refresh to see the changes</p>',
+			showConfirmButton: false,
+			timer: 1500,
 		});
 	};
 
@@ -26,7 +36,7 @@ const Homepage = (props) => {
 		<Layout>
 			<div className='grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-3 lg:grid-cols-5 m-5 gap-16 md:gap-5'>
 				{favorites.map((item, index) => (
-					<MovieCardFav key={index} img={item.poster_path} title={item.title} onClickItem={() => navigate(`/movie/${item.id}`)} onClickRemove={() => RemoveFav()} />
+					<MovieCardFav key={index} img={item.poster_path} title={item.title} onClickItem={() => navigate(`/movie/${item.id}`)} onClickRemove={() => RemoveFav(index)} />
 				))}
 			</div>
 		</Layout>
--- src/utils/redux/reducer/reducer.js
+++ src/utils/redux/reducer/reducer.js
@@ -13,6 +13,12 @@ export const reducer = (state = initialState, action) => {
 				favorites: action.payload,
 				loading: false,
 			};
+		case 'REMOVE_FAVORITE':
+			return {
+				...state,
+				favorites: state.favorites.filter((favorite) => favorite !== action.payload),
+				loading: false,
+			};
 		default:
 			return state;
 	}
